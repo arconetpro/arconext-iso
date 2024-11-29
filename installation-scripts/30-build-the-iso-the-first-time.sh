30f5b859979e6335bf48180d6b135f82392a0093
Erik Dubois <erik.dubois@gmail.com>
2024-11-29 14:18:05 +0100

    archiso

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -68,7 +68,7 @@ echo
 	isoLabel='arconext-'$arcolinuxVersion'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 81-1"
+	archisoRequiredVersion="archiso 82-1"
 	buildFolder=$HOME"/arconext-build"
 	outFolder=$HOME"/arconext-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
